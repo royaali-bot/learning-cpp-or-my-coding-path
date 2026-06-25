c134cd312e08574fcc1068304707410e880cdaa4
Royagul <royaali676@gmail.com>
2026-06-25 19:13:01 +0500

    Implement age calculation in age_calculator.cpp
    C++ Age Calculator
    A simple program built to practice declaring integer variables, performing basic math logic, and printing data to the screen using <iostream>. Part of my daily C++ learning journey.

---
+++ ​age_calculator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int birthyear = 2007;
+  int currentyear = 2026;
+  int calculatedage = currentyear - birthyear;
+  cout << calculatedage;
+  return 0;
+}
